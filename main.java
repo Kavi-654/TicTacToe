17986d2f9ab9c60d24340c4cda7d0e603dda8f7d
Kavinayasri <146553129+Kavi-654@users.noreply.github.com>
2025-11-10 18:55:06 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+package TicTacToeGame;
+
+import TicTacToeGame.view.View;
+
+public class main {
+    public static void main(String[] args) {
+        View view = new View();
+        view.startGame();
+    }
+}
